de0a5e7bad06bf4cdd1d953d175ea5125e5f3726
Jonathan Wilson <jdw7@rice.edu>
2015-05-30 14:49:47 -0500

    Added Tasks page.
    views.py:
    
    	Added route to render tasks.html. Selects from db and
    	does form validation
    	Added route for adding tasks
    	Added dynamic route for marking task as complete
    	Added dynamic route for deleting task
    
    tasks.html:
    	Form to add new task with Name, Due Date, and Priority
    	(select tag)
    	Table to display rows of open_tasks
    	Table to display rows of closed_tasks

--- project/_config.py
+++ project/_config.py
@@ -11,3 +11,7 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = '\x04\x875(\x932t\xc6+9uT!yQ\x8c\xba\xd2a\xe4\xee\xa4\xc9\x97'
 
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# print DATABASE
+# print DATABASE_PATH
+# print __file__
