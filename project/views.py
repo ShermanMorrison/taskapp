17951e6621460934a09057e65e2bcb149d0d037b
Jonathan Wilson <jdw7@rice.edu>
2015-05-30 15:05:44 -0500

    Fixed indentation in views.py /add/ route

--- project/views.py
+++ project/views.py
@@ -96,13 +96,13 @@ def new_task():
         flash("All fields are required. Please try again")
         return redirect(url_for('tasks'))
     else:
-        g.db.execute('insert into tasks (name, due_date, priority, status'
-                     'values (?,?,?,1)', [
-                         request.form['name'],
-                         request.form['due_date'],
-                         request.form['priority']
-                     ]
-        )
+        g.db.execute('insert into tasks (name, due_date, priority, status) \
+            values (?, ?, ?, 1)', [
+                request.form['name'],
+                request.form['due_date'],
+                request.form['priority']
+        ]
+)
         g.db.commit()
         g.db.close()
         flash('New entry was successfully posted.')
